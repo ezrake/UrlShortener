b98c9bb54dc31f92134ac1ee79861a79c98905b7
Ezra Akeba <akebaezra@gmail.com>
2022-05-15 20:55:58 +0300

    Set up click stats model
    - Set up click stats migration
    - Create one url to many click stats associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_084741) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_132018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "click_stats", force: :cascade do |t|
+    t.string "user_agent"
+    t.integer "clicks"
+    t.bigint "url_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["url_id"], name: "index_click_stats_on_url_id"
+  end
 
   create_table "urls", force: :cascade do |t|
     t.string "long_url"
@@ -22,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_084741) do
     t.string "full_url"
     t.datetime "expires_at"
   end
+
+  add_foreign_key "click_stats", "urls"
 end
